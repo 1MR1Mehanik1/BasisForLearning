44badd12c8e39cbca04a5e2bd8cc2c4edb2a6384
MR Mehanik <58657592+1MR1Mehanik1@users.noreply.github.com>
2025-01-02 19:18:48 +0300

    new load

--- src/main/java/basis_for_learning/init/ItemsInit.java
+++ src/main/java/basis_for_learning/init/ItemsInit.java
@@ -1,9 +1,12 @@
 package basis_for_learning.init;
 
+import basis_for_learning.init.basis.items.*;
 import net.minecraft.item.*;
 
 import java.util.*;
 
 public class ItemsInit {
     public static final List<Item> ITEMS = new ArrayList<Item>();
+
+    public static final Item TEST_ITEM = new BasicItem("test_item", 16);
 }
